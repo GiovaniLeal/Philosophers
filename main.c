888c69fe63c138331d8935558c0e21c81ff16687
Giovani Dos Santos De Andrade Leal <giodos-s@c3r1p5.42sp.org.br>
2026-05-09 18:12:41 -0300

    style: Molhoria da identação conforme com a Norminette e de strucs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: giodos-s <giodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 19:23:11 by giodos-s          #+#    #+#             */
-/*   Updated: 2026/05/07 20:06:43 by giodos-s         ###   ########.fr       */
+/*   Updated: 2026/05/09 17:35:21 by giodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 void	arg_error_msg(void)
 {
 	printf("ARGUMENT ERROR\n");
+	printf("===================================================\n");
 	printf(" Insert the follow arguments :\n");
 	printf(" <numb_philo> <time to die> <time to eat> <time to sleep>");
 	printf("<numb of times each philo must eat>\n");
+	print("\n");
+	printf(" ** The number of philosophers not pass 200");
+	printf("===================================================\n");
 }
 
 
@@ -29,14 +33,24 @@ int	main(int argc, char *argv)
 	i = 0;
 	if (argc > 5 || argc < 5)
 		arg_error_msg();
-	while (argv[i])
+	if (check_arguments(argv))
 	{
-		check_arguments(argv, &philo);
+		arg_error_msg();
+		return (1);
 	}
+	create_thread();
 	return(0);
 }
 
-void	check_arguments(char *argv, t_philo *philo)
+int	check_arguments(char *argv)
 {
+	int	i;
 
+	i= 0;
+	while (argv[i])
+	{
+		if (argv[i] > 200)
+			return (1);
+	}
+	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: giodos-s <giodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 19:29:27 by giodos-s          #+#    #+#             */
-/*   Updated: 2026/05/07 19:58:02 by giodos-s         ###   ########.fr       */
+/*   Updated: 2026/05/09 18:06:00 by giodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,30 @@
 # include <sys/time.h> //? gettimeofday 
 
 /* ************************************************************************** */
-//*                         PHILOSOPHER STRUCT                                */
+//*                      RULES AND PHILOSOPHERS STRUCTS                       */
 /* ************************************************************************** */
-typedef struct s_philo{
-              int           t_eat;
-              int           t_sleep;
-              int           t_think;
-              int           t_die;
-              int           numb_of_philos;
-              int           numb_of_each_philo_eat;
-}	t_philo;
+typedef struct s_rules
+{
+	int				t_eat;
+	int				t_sleep;
+	int				t_think;
+	int				t_die;
+	int				num_philos;
+	int				must_eat;
+	int				someone_died;
+	long long		start_time;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*print_lock;
+}	t_rules;
 
+typedef struct s_philo
+{
+	int				id;
+	int				meals_eaten;
+	t_rules			*rules;
+	pthread_t		thread;
+	long long		last_meal;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}	t_philo;
 #endif
